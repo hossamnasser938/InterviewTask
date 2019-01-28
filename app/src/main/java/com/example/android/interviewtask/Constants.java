44a3b631d9b2a818cb5d97e4c2fb298bc6fdd259
Hosam <hossamnasser938@gmail.com>
2019-01-28 22:29:20 +0200

    Put Constants class in the right package

--- app/src/main/java/com/example/android/interviewtask/Constants.java
+++ app/src/main/java/com/example/android/interviewtask/Constants.java
@@ -1,3 +1,5 @@
+package com.example.android.interviewtask;
+
 public class Constants {
 
     public static final Integer RC_SIGN_IN = 100;
--- app/src/main/java/com/example/android/interviewtask/SignInActivity.kt
+++ app/src/main/java/com/example/android/interviewtask/SignInActivity.kt
@@ -1,20 +1,19 @@
 package com.example.android.interviewtask
 
-import Constants.RC_SIGN_IN
 import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.android.interviewtask.Constants.RC_SIGN_IN
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
 
-    lateinit var firebaseAuth: FirebaseAuth
-
     val TAG = "SignInActivity"
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
